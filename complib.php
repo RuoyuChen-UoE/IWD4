b3bd6868a7d237e804a518212b72812254d29790
Ruoyu Chen <s2552113@bioinfmsc8.bio.ed.ac.uk>
2024-03-20 19:39:32 +0000

    change pdo connection

--- complib.php
+++ complib.php
@@ -7,9 +7,34 @@ echo<<<_HEAD1
 _HEAD1;
 
 // THE CONNECTION AND QUERY SECTIONS NEED TO BE MADE TO WORK FOR PHP 8 USING PDO... //
+// Connect to the database using PDO
+try {
+   $dsn = "mysql:host=$hostname;dbname=$database;charset=utf8";
+   $options = [
+       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,// Set error mode to exceptions
+       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,// Set the default fetch mode of PDO 'fetch()' method -- return an associative array
+       PDO::ATTR_EMULATE_PREPARES   => false// Use native prepared statements
+   ];
+   $pdo = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+   die("Failed to connect to database: " . $e->getMessage());
+}
+
+
+
+
+
+
+
+
+
+
+
+//finished
 $db_server = mysql_connect($db_hostname,$db_username,$db_password);
 if(!$db_server) die("Unable to connect to database: " . mysql_error());
 mysql_select_db($db_database,$db_server) or die ("Unable to select database: " . mysql_error());     
+
 $query = "select * from Manufacturers";
      $result = mysql_query($query);
      if(!$result) die("unable to process query: " . mysql_error());
