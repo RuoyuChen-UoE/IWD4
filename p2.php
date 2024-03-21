edf42f9bd8f6661e0f608f5b6fe6984fcc7f51d3
Ruoyu Chen <s2552113@bioinfmsc8.bio.ed.ac.uk>
2024-03-21 05:30:40 +0000

    p2.php invalid parameter number

--- p2.php
+++ p2.php
@@ -2,7 +2,6 @@
 session_start();
 require_once 'login.php';
 require_once 'dbconnect.php';// Contains the database connection
-
 include 'redir.php';
 include 'menuf.php';
 
@@ -10,35 +9,6 @@ echo<<<_HEAD1
 <html>
 <body>
 _HEAD1;
-
-// THE CONNECTION AND QUERY SECTIONS NEED TO BE MADE TO WORK FOR PHP 8 USING PDO... //
-// $db_server = mysql_connect($db_hostname,$db_username,$db_password);
-// if(!$db_server) die("Unable to connect to database: " . mysql_error());
-// mysql_select_db($db_database,$db_server) or die ("Unable to select database: " . mysql_error());     
-// $query = "select * from Manufacturers";
-// $result = mysql_query($query);
-// if(!$result) die("unable to process query: " . mysql_error());
-// $rows = mysql_num_rows($result);
-// $smask = $_SESSION['supmask'];
-
-// $firstmn = False;
-// $mansel = "(";
-// for($j = 0 ; $j < $rows ; ++$j)
-//   {
-//     $row = mysql_fetch_row($result);
-//     $sid[$j] = $row[0];
-//     $snm[$j] = $row[1];
-//     $sact[$j] = 0;
-//     $tvl = 1 << ($sid[$j] - 1);
-//     if($tvl == ($tvl & $smask)) {
-// 	$sact[$j] = 1;
-	// if($firstmn) $mansel = $mansel." or ";
-	// $firstmn = True;
-	// $mansel = $mansel." (ManuID = ".$sid[$j].")";
-  //     }
-  // }
-// $mansel = $mansel.")";
-// $setpar = isset($_POST['natmax']); 
 
 // Query Manufacturer information
 try {
@@ -48,15 +18,14 @@ try {
   $rows = $result->fetchAll(PDO::FETCH_ASSOC);//Gets all rows as an associative array
   $smask = $_SESSION['supmask'];
 
+
+  $mansel = "(";//Limit query results to contain only compounds from a specific manufacturer
   $firstmn = False;
-  $mansel = "(";
-  
-  foreach ($result->fetchAll(PDO::FETCH_ASSOC) as $row) {
+  foreach ($rows as $row) {
     $sid[] = $row['id']; 
     $snm[] = $row['name'];
     $sact[] = 0;
     $tvl = 1 << ($row['id'] - 1); // Calculate the bit value for this supplier
-   
     if ($tvl == ($tvl & $smask)) { // Check if this supplier is selected
         //$sact[count($sact) - 1] = 1; // Update the activation status
         if ($firstmn) {
@@ -64,9 +33,11 @@ try {
         } else {
           $firstmn = True;
         }
-        $mansel .= " (ManuID = $sid)";
+        $mansel .= " (ManuID = " . $row['id'] . ")";
     }
-    $mansel = $mansel.")";
+  }
+  if ($firstmn) {
+    $mansel .= ")"; // Add closing parentheses only at the end
   }
 } catch (PDOException $e) {
   die("Failed to execute query: " . $e->getMessage());
@@ -80,50 +51,84 @@ This is the catalogue retrieval Page
 _MAIN1;
 
 $setpar = isset($_POST['natmax']); //Check if specific POST parameters are set
+$firstsl = False;//Initializes $firstsl outside of a conditional statement
+
+//Check if a condition is added and build $compsel
 if($setpar) {
-  $firstsl = False;
-  $compsel = "select catn from Compounds where (";
-  if (($_POST['natmax'] != "") && ($_POST['natmin']!="")) {
-    $compsel = $compsel."(natm > ".get_post('natmin')." and  natm < ".get_post('natmax').")";
-    $firstsl = True;
+  //Initializes the query criteria and parameter array
+  $conditions = [];
+  $params = [];  
+  //Build query conditions and parameters based on user input
+  if (!empty($_POST['natmax']) && !empty($_POST['natmin'])) {
+    $conditions[] = "(natm > :natmin AND natm < :natmax)";
+    $params[':natmin'] = $_POST['natmin'];
+    $params[':natmax'] = $_POST['natmax'];  
   }
-  if (($_POST['ncrmax']!="") && ($_POST['ncrmin']!="")) {
-    if($firstsl) $compsel = $compsel." and ";
-    $compsel = $compsel."(ncar > ".get_post('ncrmin')." and  ncar < ".get_post('ncrmax').")";
-    $firstsl = True;
+
+  if (!empty($_POST['ncrmax']) && !empty($_POST['ncrmin'])) {
+    $conditions[] = "(ncar > :ncrmin AND ncar < :ncrmax)";
+    $params[':ncrmin'] = $_POST['ncrmin'];
+    $params[':ncrmax'] = $_POST['ncrmax'];
   }
-  if (($_POST['nntmax']!="") && ($_POST['nntmin']!="")) {
-    if($firstsl) $compsel = $compsel." and ";
-    $compsel = $compsel."(nnit > ".get_post('nntmin')." and  nnit < ".get_post('nntmax').")";
-    $firstsl = True;
+
+  if (!empty($_POST['nntmax']) && !empty($_POST['nntmin'])) {
+      $conditions[] = "(nnit > :nntmin AND nnit < :nntmax)";
+      $params[':nntmin'] = $_POST['nntmin'];
+      $params[':nntmax'] = $_POST['nntmax'];
   }
-  if (($_POST['noxmax']!="") && ($_POST['noxmin']!="")) {
-    if($firstsl) $compsel = $compsel." and ";
-    $compsel = $compsel."(noxy > ".get_post('noxmin')." and  noxy < ".get_post('noxmax').")";
-    $firstsl = True;
+
+  if (!empty($_POST['noxmax']) && !empty($_POST['noxmin'])) {
+      $conditions[] = "(noxy > :noxmin AND noxy < :noxmax)";
+      $params[':noxmin'] = $_POST['noxmin'];
+      $params[':noxmax'] = $_POST['noxmax'];
   }
+  // $firstsl should be set to True only if there is a condition
+  if (count($conditions) > 0) {
+    $firstsl = True;
+    $compsel = "SELECT * FROM Compounds WHERE " . implode(" AND ", $conditions);
+    if ($firstmn) { // If there is a selected manufacturer, it is added to the query
+      $compsel .= " AND " . $mansel;
+    }
+  }
+}
+
+
+echo '<pre>';
+var_dump($_POST);
+echo '</pre>';
+
+echo '<pre>';
+var_dump($params);
+echo '</pre>';
+
   echo "<pre>";
   if($firstsl) {
-    $compsel = $compsel.") and ".$mansel;
-    echo $compsel;
-    echo "\n";
-     $result = mysql_query($compsel);
-     if(!result) die("unable to process query: " . mysql_error());
-     $rows = mysql_num_rows($result);
-     if($rows > 100) {
-       echo "Too many results ",$rows," Max is 100\n";
-     } else  {
-     for($j = 0 ; $j < $rows ; ++$j)
-       {
-	 $row = mysql_fetch_row($result);
-	 echo $row[0],"\n";
-       }
-     }
+    // $compsel = $compsel.") and ".$mansel;//End the condition statement and add the manufacturer condition
+    // echo $compsel . "\n";
+    try {
+    // Execute queries using PDO
+        $result =$pdo->query($compsel);
+        $rows = $result->fetchAll(PDO::FETCH_ASSOC);
+
+        $rowCount = count($rows);//count the lines of the rows
+        if($rows > 100) {
+          echo "Too many results ",$rows," Max is 100\n";
+        } else  {
+          foreach ($rows as $row) {
+            echo $row['catn'] . "\n";
+          }
+        }
+      } catch (PDOException $e) {
+        die("Unable to process query: " . $e->getMessage());
+      }
   } else {
     echo "No Query Given\n";
-  }
+  }        
   echo "</pre>";
-} 
+
+
+
+
 echo <<<_TAIL1
    <form action="p2.php" method="post"><pre>
        Max Atoms      <input type="text" name="natmax"/>    Min Atoms    <input type="text" name="natmin"/>
@@ -136,8 +141,8 @@ echo <<<_TAIL1
 </body>
 </html>
 _TAIL1;
-function get_post($var)
+function get_post( $var)
 {
-  return mysql_real_escape_string($_POST[$var]);
+    return $_POST[$var];
 }
 ?>
