3dd22e79a83f89620dd8a3423ff2d06f9a4b04e2
Ruoyu Chen <s2552113@bioinfmsc8.bio.ed.ac.uk>
2024-03-22 03:41:22 +0000

    change some coomment

--- dbconnect.php
+++ dbconnect.php
@@ -2,14 +2,17 @@
 // dbconnect.php
 // Connect to the database using PDO
 try {
-   $dsn = "mysql:host=$hostname;dbname=$database;charset=utf8";
+   $conn = "mysql:host=$hostname;dbname=$database;charset=utf8";
    $options = [
        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,// Set error mode to exceptions
        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,// Set the default fetch mode of PDO 'fetch()' method -- return an associative array
-       PDO::ATTR_EMULATE_PREPARES   => false// Use native prepared statements
+       PDO::ATTR_EMULATE_PREPARES   => false// Use native prepared statements to prevents SQL injection attacks
    ];
-   $pdo = new PDO($dsn, $username, $password, $options);
+   $pdo = new PDO($conn, $username, $password, $options);
+   echo "Connected successfully";  // Used for debugging, should be removed in practical applications
+   //here can add some database manipulation
 } catch (PDOException $e) {
    die("Failed to connect to database: " . $e->getMessage());
 }
+
 ?>
