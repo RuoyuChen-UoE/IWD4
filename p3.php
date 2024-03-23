b5e0be620cee7681b3091ee17453461f64706404
Ruoyu Chen <s2552113@bioinfmsc8.bio.ed.ac.uk>
2024-03-23 13:35:04 +0000

    successsssssgit add p3.php ! p3.php

--- p3.php
+++ p3.php
@@ -2,6 +2,7 @@
 session_start();
 include 'redir.php';
 require_once 'login.php';
+require_once 'dbconnect.php';// Contains the database connection
 echo<<<_HEAD1
 <html>
 <body>
@@ -30,17 +31,28 @@ if(isset($_POST['tgval']))
      } 
      // Display the chosen field and its human-readable name
      printf(" Statistics for %s (%s)<br />\n",$dbfs[$chosen],$nms[$chosen]);
-// THE CONNECTION AND QUERY SECTIONS NEED TO BE MADE TO WORK FOR PHP 8 USING PDO... //
-     //Your mysql and statistics calculation goes here
-     $db_server = mysql_connect($db_hostname,$db_username,$db_password);
-     if(!$db_server) die("Unable to connect to database: " . mysql_error());
-     mysql_select_db($db_database,$db_server) or die ("Unable to select database: " . mysql_error());     
-     $query = sprintf("select AVG(%s), STD(%s) from Compounds",$dbfs[$chosen],$dbfs[$chosen]);
-     $result = mysql_query($query);
-     if(!$result) die("unable to process query: " . mysql_error());
-     $row = mysql_fetch_row($result);
-     printf(" Average %f  Standard Dev %f <br />\n",$row[0],$row[1]);
-   }
+
+    // connect  the database, file included in dbconnection
+    //query from Compounds
+    try {
+      $query="SELECT AVG($dbfs[$chosen]), STD($dbfs[$chosen]) FROM Compounds";
+      $stmt= $pdo -> prepare($query);
+      $stmt->execute();// execute query
+      $result = $stmt->fetch(PDO::FETCH_NUM);
+      $stmt->closeCursor();
+
+      if ($result) {
+        // If the query is successful and returns results, use the printf function to format and output the mean and standard deviation
+        printf("Average: %f  Standard Dev: %f <br />\n", $result[0], $result[1]);
+    } else {
+      // If the query succeeds but returns no results (e.g., the query table is empty), output "No data found".
+        echo "No data found.<br />\n";
+    }
+} catch (PDOException $e) {
+    die("Failed to execute query: " . $e->getMessage());
+    }
+  }
+
 // Form for selecting a database field to view statistics
 echo '<form action="p3.php" method="post"><pre>';
 for($j = 0 ; $j <sizeof($dbfs) ; ++$j) {//Use a for loop to dynamically generate form elements based on the number of elements in the array.
