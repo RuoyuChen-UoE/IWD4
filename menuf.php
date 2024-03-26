6788c5d699307848b53ef727ddebaf144e69ba9c
Ruoyu Chen <s2552113@bioinfmsc8.bio.ed.ac.uk>
2024-03-26 23:57:25 +0000

    add smile table cell

--- menuf.php
+++ menuf.php
@@ -5,20 +5,36 @@ echo <<<_MENU1
    <td bgcolor="#DCEFFE"><div align="center">
     <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2552113/p1.php"> Select Suppliers </a>
     </div></td>
-   <td bgcolor="#DCEFFE"><div align="center">
+   
+    <td bgcolor="#DCEFFE"><div align="center">
     <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2552113/p2.php"> Search Compounds </a>
     </div></td>
-   <td bgcolor="#DCEFFE"><div align="center">
+   
+
    <td bgcolor="#DCEFFE"><div align="center">
     <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2552113/p3.php"> Stats </a>
     </div></td>
-   <td bgcolor="#DCEFFE"><div align="center">
+   
    <td bgcolor="#DCEFFE"><div align="center">
     <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2552113/p4.php"> Correlations </a>
     </div></td>
-   <td bgcolor="#DCEFFE"><div align="center">
+   
+    <td bgcolor="#DCEFFE"><div align="center">
     <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2552113/p5.php"> Exit </a>
     </div></td>
+
+    <td bgcolor="#DCEFFE"><div align="center">
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2552113/smile_draw.php"> Draw SMILE </a>
+    </div></td>
+
+    <td bgcolor="#DCEFFE"><div align="center">
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2552113/smile_draw_NIH.php"> Draw SMILE_NIH </a>
+    </div></td>
+   
+    <td bgcolor="#DCEFFE"><div align="center">
+    <a href="https://bioinfmsc8.bio.ed.ac.uk/~s2552113/phelp.php"> About this website </a>
+    </div></td> 
+    
     </tr></table>
 _MENU1;
 ?>
