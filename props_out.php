72e33ff97df358cf028f21deb1ccdbeef8a77614
Ruoyu Chen <s2552113@bioinfmsc8.bio.ed.ac.uk>
2024-04-01 19:06:04 +0100

    add pages

--- props_out.php
+++ props_out.php
@@ -6,6 +6,21 @@ require_once 'dbconnect.php';
 include 'menuf.php';
 echo <<<_HEAD1
 <html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<script src="https://unpkg.com/smiles-drawer@2.0.1/dist/smiles-drawer.min.js"></script>
+<script>
+function getAndDrawSmiles(smiles) {
+    var smilesDrawer = new SmilesDrawer.Drawer({ width: 300, height: 200 });
+    SmilesDrawer.parse(smiles, function(tree) {
+        smilesDrawer.draw(tree, 'smiles-canvas', 'light', false);
+    }, function (err) {
+        console.error(err);
+    });
+}
+</script>
+<canvas id="smiles-canvas" width="300" height="200"></canvas>
 <body>
 _HEAD1;
 
@@ -19,6 +34,7 @@ try {
   $stmt->execute();
   $results = $stmt->fetchAll();
   $rows = count($results);
+
   $stmt->closeCursor();
 
   for ($j = 0; $j < $rows; ++$j) {
@@ -36,16 +52,49 @@ This is the initial property retrieval page
     </pre>
 _MAIN1;
 
+if (isset($_POST['tgval']) && isset($_POST['cval'])) {
+  $_SESSION['tgval'] = $_POST['tgval'];
+  $_SESSION['cval'] = $_POST['cval'];
+}
+// Paging parameters
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$pageSize = 30; // number of items per page
+$offset = ($page - 1) * $pageSize;
+
+
 
 // Here we generate our query
-if (($_POST['tgval'] != "") && ($_POST['cval']!="")) {
-    $mychoice=get_post('tgval');
-    $myvalue=get_post('cval');
+if (isset($_SESSION['tgval']) && $_SESSION['tgval'] != "" && isset($_SESSION['cval']) && $_SESSION['cval'] != "") {
+  $mychoice = $_SESSION['tgval'];
+  $myvalue = $_SESSION['cval'];
+    
 
-
+    // total query
+    $totalQuery = "SELECT COUNT(*) FROM Compounds ";
+    $totalQuery .= "LEFT JOIN Smiles ON Compounds.id = Smiles.cid ";
+    
+    if($mychoice == "mw") {
+        $totalQuery .= "WHERE mw > ".($myvalue - 1.0)." AND mw < ".($myvalue + 1.0);
+    }
+    if($mychoice == "TPSA") {
+        $totalQuery .= "WHERE TPSA > ".($myvalue - 0.1)." AND TPSA < ".($myvalue + 0.1);
+    }
+    if($mychoice == "XlogP") {
+        $totalQuery .= "WHERE XlogP > ".($myvalue - 0.1)." AND XlogP < ".($myvalue + 0.1);
+    }
+    
+    // Execute a query to get the total number of records
+    // Execute a query to get the total number of records
+    $stmt = $pdo->query($totalQuery);
+    $totalRows = $stmt->fetchColumn(); 
+    $totalPages = ceil($totalRows / $pageSize); // calculate total pages
+    echo"total rows:$totalRows";
+    echo"total pages: $totalPages";
+    echo"page size: $pageSize";
     // $compsel = "select * from Compounds where ";
     $compsel = "SELECT Compounds.*, Smiles.smiles FROM Compounds ";
     $compsel .= "LEFT JOIN Smiles ON Compounds.id = Smiles.cid ";
+
     if($mychoice == "mw") {
         $compsel .= "WHERE mw > ".($myvalue - 1.0)." AND mw < ".($myvalue + 1.0);
     }
@@ -56,19 +105,10 @@ if (($_POST['tgval'] != "") && ($_POST['cval']!="")) {
         $compsel .= "WHERE XlogP > ".($myvalue - 0.1)." AND XlogP < ".($myvalue + 0.1);
     }
     
+    $compsel .= " LIMIT $pageSize OFFSET $offset";//modify sql query to add limit and offset
     
-    
-    // if($mychoice == "mw") {
-    //   $compsel = $compsel."( mw > ".($myvalue - 1.0)." and  mw < ".($myvalue + 1.0).")";
-    // }
-    // if($mychoice == "TPSA") {
-    //   $compsel = $compsel."( TPSA > ".($myvalue - 0.1)." and  TPSA < ".($myvalue + 0.1).")";
-    // }
-    // if($mychoice == "XlogP") {
-    //   $compsel = $compsel."( XlogP > ".($myvalue - 0.1)." and  XlogP < ".($myvalue + 0.1).")";
-    // }
     echo "<pre>";
-       echo $compsel;//debug
+    echo $compsel;//debug
     echo "\n";
     try {
       $stmt = $pdo->query($compsel);
@@ -76,12 +116,14 @@ if (($_POST['tgval'] != "") && ($_POST['cval']!="")) {
       $results = $stmt->fetchAll();
       $rows = count($results);
       $stmt->closeCursor();
+
+
       // print_r($results); //debug
     } catch(PDOException $e) {
         die("Unable to process query: " . $e->getMessage());
     }
 
- if($rows > 10000) {
+if($rows > 10000) {
       echo "Too many results ",$rows," Max is 10000\n";
     } else  {
       echo <<<TABLESET_
@@ -90,6 +132,7 @@ if (($_POST['tgval'] != "") && ($_POST['cval']!="")) {
         <td>CAT Number</td>
         <td>Manufacturer</td>
         <td>Property</td>
+        <td>Structure</td>
       </tr>
     TABLESET_;
 
@@ -99,7 +142,7 @@ if (($_POST['tgval'] != "") && ($_POST['cval']!="")) {
 
       echo "<tr>";
 
-    
+
       printf("<td>%s</td> <td>%s</td>", $row['catn'],$manarray[$row['ManuID'] - 1]);
       if($mychoice == "mw") {
         printf("<td>%s</td> ", $row['mw']);
@@ -109,11 +152,96 @@ if (($_POST['tgval'] != "") && ($_POST['cval']!="")) {
       }
       if($mychoice == "XlogP") {
         printf("<td>%s</td> ", $row['XLogP']);
-      }     
+      }
+      // Add smile drawing button
+      echo "<td><button onclick=\"getAndDrawSmiles('{$row['smiles']}')\">Draw Structure</button></td>";     
             echo "</tr>";
+            
       }
         echo "</table>";
       }
+      // echo "<div id='pagination'>";
+      // for ($i = 1; $i <= $totalPages; $i++) {
+      //   echo "<a href='?page=$i'>$i</a> ";
+      // }
+      echo "</div>";
+
+
+      echo <<<_STYLE
+      <style>
+      #pagination {
+          font-family: 'Arial', sans-serif; /* Change the font */
+          font-size: 14px; /* Change font size */
+          color: #333; /* Font color */
+          text-align: center; /* Center the pagination */
+          margin: 20px 0; /* Add some top and bottom margin */
+      }
+      
+      #pagination a {
+          text-decoration: none; /* Remove underline */
+          margin: 0 5px; /* Space on the sides */
+          padding: 5px 10px; /* Padding */
+          color: #337ab7; /* Link color */
+          border: 1px solid #ddd; /* Border */
+          background-color: #f8f8f8; /* Background color */
+      }
+      
+      #pagination a:hover {
+          background-color: #e9e9e9; /* Background color on hover */
+      }
+      
+      #pagination a.active {
+          background-color: #337ab7; /* Background color for the active link */
+          color: white; /* Font color for the active link */
+          border-color: #337ab7; /* Border color for the active link */
+      }
+      </style>
+      _STYLE;
+
+      echo "<div id='pagination'>";
+
+      // Previous button
+      if ($page > 1) {
+        echo "<a href='?page=" . ($page - 1) . "'>< Previous</a> ";
+      }
+      // Always show the first page
+      echo "<a href='?page=1'>1</a> ";
+      // If there are more than 5 pages, add "..."
+      if ($totalPages > 5) {
+          echo " ... ";
+      }
+      
+      // Show some pages before current page
+      for ($i = max(2, $page - 2); $i < $page; $i++) {
+          echo "<a href='?page=$i'>$i</a> ";
+      }
+      
+      // Show the current page
+      if ($page > 1 && $page < $totalPages) {
+          echo "<a href='?page=$page'>$page</a> ";
+      }
+      
+      // Show some pages after current page
+      for ($i = $page + 1; $i < min($page + 3, $totalPages); $i++) {
+          echo "<a href='?page=$i'>$i</a> ";
+      }
+      
+      // If there's a gap between the loops and the last page, add "..."
+      if ($totalPages - $page > 3) {
+          echo " ... ";
+      }
+      
+      // Always show the last page
+      if ($page < $totalPages) {
+          echo "<a href='?page=$totalPages'>$totalPages</a> ";
+      }
+      
+      // Next button
+      if ($page < $totalPages) {
+          echo "<a href='?page=" . ($page + 1) . "'>Next ></a>";
+      }
+      
+      echo "</div>";
       } else {
     echo "No Query Given\n";
   }
