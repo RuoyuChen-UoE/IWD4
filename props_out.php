224d5fcfdd58489d8d34a1653701cbedbf3394fd
Ruoyu Chen <s2552113@bioinfmsc8.bio.ed.ac.uk>
2024-04-01 13:05:35 +0100

    join with smile

--- props_out.php
+++ props_out.php
@@ -3,11 +3,12 @@ session_start();
 require_once 'login.php';
 include 'redir.php';
 require_once 'dbconnect.php';
+include 'menuf.php';
 echo <<<_HEAD1
 <html>
 <body>
 _HEAD1;
-include 'menuf.php';
+
 
 $manarray = array();  //build an empty array to store manufacture query
 try {
@@ -40,16 +41,32 @@ _MAIN1;
 if (($_POST['tgval'] != "") && ($_POST['cval']!="")) {
     $mychoice=get_post('tgval');
     $myvalue=get_post('cval');
-    $compsel = "select * from Compounds where ";
+
+
+    // $compsel = "select * from Compounds where ";
+    $compsel = "SELECT Compounds.*, Smiles.smiles FROM Compounds ";
+    $compsel .= "LEFT JOIN Smiles ON Compounds.id = Smiles.cid ";
     if($mychoice == "mw") {
-      $compsel = $compsel."( mw > ".($myvalue - 1.0)." and  mw < ".($myvalue + 1.0).")";
+        $compsel .= "WHERE mw > ".($myvalue - 1.0)." AND mw < ".($myvalue + 1.0);
     }
     if($mychoice == "TPSA") {
-      $compsel = $compsel."( TPSA > ".($myvalue - 0.1)." and  TPSA < ".($myvalue + 0.1).")";
+        $compsel .= "WHERE TPSA > ".($myvalue - 0.1)." AND TPSA < ".($myvalue + 0.1);
     }
     if($mychoice == "XlogP") {
-      $compsel = $compsel."( XlogP > ".($myvalue - 0.1)." and  XlogP < ".($myvalue + 0.1).")";
+        $compsel .= "WHERE XlogP > ".($myvalue - 0.1)." AND XlogP < ".($myvalue + 0.1);
     }
+    
+    
+    
+    // if($mychoice == "mw") {
+    //   $compsel = $compsel."( mw > ".($myvalue - 1.0)." and  mw < ".($myvalue + 1.0).")";
+    // }
+    // if($mychoice == "TPSA") {
+    //   $compsel = $compsel."( TPSA > ".($myvalue - 0.1)." and  TPSA < ".($myvalue + 0.1).")";
+    // }
+    // if($mychoice == "XlogP") {
+    //   $compsel = $compsel."( XlogP > ".($myvalue - 0.1)." and  XlogP < ".($myvalue + 0.1).")";
+    // }
     echo "<pre>";
        echo $compsel;//debug
     echo "\n";
@@ -68,36 +85,36 @@ if (($_POST['tgval'] != "") && ($_POST['cval']!="")) {
       echo "Too many results ",$rows," Max is 10000\n";
     } else  {
       echo <<<TABLESET_
-<table border="1">
-  <tr>
-    <td>CAT Number</td>
-    <td>Manufacturer</td>
-    <td>Property</td>
-  </tr>
-TABLESET_;
+    <table border="1">
+      <tr>
+        <td>CAT Number</td>
+        <td>Manufacturer</td>
+        <td>Property</td>
+      </tr>
+    TABLESET_;
 
 // This is the results processing section, which also needs to be recoded for PHP 8 PDO 
-      for($j = 0 ; $j < $rows ; ++$j)
+      for($j = 0 ; $j < $rows ; ++$j){
       $row = $results[$j];
-  {
-    echo "<tr>";
 
-   
-    printf("<td>%s</td> <td>%s</td>", $row['catn'],$manarray[$row['ManuID'] - 1]);
-    if($mychoice == "mw") {
-       printf("<td>%s</td> ", $row['mw']);
-    }
-    if($mychoice == "TPSA") {
-       printf("<td>%s</td> ", $row['TPSA']);
-    }
-    if($mychoice == "XlogP") {
-       printf("<td>%s</td> ", $row['XLogP']);
-    }     
-          echo "</tr>";
-  }
-      echo "</table>";
-    }
-  } else {
+      echo "<tr>";
+
+    
+      printf("<td>%s</td> <td>%s</td>", $row['catn'],$manarray[$row['ManuID'] - 1]);
+      if($mychoice == "mw") {
+        printf("<td>%s</td> ", $row['mw']);
+      }
+      if($mychoice == "TPSA") {
+        printf("<td>%s</td> ", $row['TPSA']);
+      }
+      if($mychoice == "XlogP") {
+        printf("<td>%s</td> ", $row['XLogP']);
+      }     
+            echo "</tr>";
+      }
+        echo "</table>";
+      }
+      } else {
     echo "No Query Given\n";
   }
 echo "</pre>"; 
